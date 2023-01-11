c1a3a48c5af62bde2e85d5c31296e24d42d0f9a7
Rikellyh <rikellyh898@gmail.com>
2023-01-11 09:34:48 -0300

    rm/feat/products: create entities

---
+++ src/entities/products.entities.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  quantity: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deleteAt: Date;
+}
+
+export { Product };
