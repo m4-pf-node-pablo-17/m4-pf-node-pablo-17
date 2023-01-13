cf7c005ce6e83282c7d001c435c0108580e37bc9
Rikellyh <rikellyh898@gmail.com>
2023-01-13 15:24:31 -0300

    rm/feat/users: create tests

---
+++ src/__tests__/integration/user/list.route.test.ts
@@ -0,0 +1,77 @@
+import request from 'supertest';
+import { sign } from 'jsonwebtoken';
+import { DataSource } from 'typeorm';
+import { User } from '../../../entities/user.entity';
+import { AppDataSource } from '../../../data-source';
+import { mockedListUsers } from '../../mocks/integration/user.mock';
+import { app } from '../../../app';
+
+describe('Create user route tests', () => {
+  let conn: DataSource;
+  const baseUrl: string = '/users';
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then(async (res) => {
+        conn = res;
+        const userRepo = res.getRepository(User);
+        for await (const user of mockedListUsers) {
+          const mockedUser = userRepo.create({
+            ...user,
+            password: 'testing',
+          });
+          await userRepo.save(mockedUser);
+        }
+      })
+      .catch((err) => console.error(err));
+  });
+
+  afterAll(async () => {
+    await conn.destroy();
+  });
+
+  it('Should be able to list users', async () => {
+    const token = sign(
+      { isStore: true || false, isActive: true },
+      process.env.SECRET_KEY,
+      {
+        subject: '1',
+      }
+    );
+
+    const response = await request(app)
+      .get(baseUrl)
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    const expectedResponse = {
+      status: 200,
+      bodyToHaveLength: mockedListUsers.length,
+    };
+
+    expect(response.status).toBe(expectedResponse.status);
+    expect(response.body).toHaveLength(expectedResponse.bodyToHaveLength);
+    expect(response.body).not.toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ password: expect.any(String) }),
+      ])
+    );
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: expect.any(String) }),
+      ])
+    );
+  });
+
+  it('Should be not able to list users | missing token', async () => {
+    const response = await request(app).get(baseUrl).send();
+
+    const expectedResponse = {
+      status: 401,
+      bodyToEqual: { message: 'Invalid token' },
+    };
+
+    expect(response.status).toBe(expectedResponse.status);
+    expect(response.body).toStrictEqual(expectedResponse.bodyToEqual);
+  });
+});
---
+++ src/__tests__/mocks/integration/user.mock.ts
@@ -0,0 +1,31 @@
+const mockedListUsers = [
+  {
+    image: 'https://source.unsplash.com/random',
+    name: 'Jolyne Kujo',
+    email: 'stoneocean@gmail.com',
+    password: 'stonefree123',
+    contact: '81997142273',
+    register: '37213020294',
+    isStore: false,
+  },
+  {
+    image: 'https://source.unsplash.com/random',
+    name: 'Usagi Tsukino',
+    email: 'naoeasailormoon@mail.com',
+    password: 'lunaeartemis16',
+    contact: '21988051506',
+    register: '00776574/0001-56',
+    isStore: true,
+  },
+  {
+    image: 'https://source.unsplash.com/random',
+    name: 'Sakura Kinomoto',
+    email: 'sakurakawaii@mail.com',
+    password: 'mahoushoujo1',
+    contact: '85995130224',
+    register: '12682833754',
+    isStore: false,
+  },
+];
+
+export { mockedListUsers };
